563e9e2f2668b9467b76e1faedbbecce2b5c9e7e
Uiguuna Mikhailova <uyguuna1997@yandex.ru>
2022-05-16 01:49:21 +0300

    feat: add solution on copy-direction  task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'files');
+const copyFolder = path.join(__dirname, 'files-copy');
+
+fs.promises.mkdir(copyFolder, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(folder, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  fs.readdir(copyFolder, (err, copies) => {
+    if (err) throw err;
+    copies.forEach(copy => {
+      fs.unlink(path.join(copyFolder, copy), err => {
+        if (err) throw err;
+      });
+    });
+    files.forEach(file => {
+      let name = file.name;
+      const input = fs.createReadStream(`./04-copy-directory/files/${name}`, 'utf-8');
+      const output = fs.createWriteStream(`./04-copy-directory/files-copy/${name}`);
+      input.pipe(output);
+    });
+  });
+});
 
