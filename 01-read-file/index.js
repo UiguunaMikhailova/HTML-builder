0b737011015ae4ec17057a7a3e6e0c4b9bc8eae0
Uiguuna Mikhailova <uyguuna1997@yandex.ru>
2022-05-17 03:34:53 +0300

    feat: change logic of read-file task, add exit on write-file task, add solution on merge-styles task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,10 @@
-const path = require('path');
 const fs = require('fs');
-fs.readFile(path.join(__dirname, 'text.txt'),'utf-8', function(error,data){
-  if(error) throw error;
-  console.log(data);
-});
+const path = require('path');
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
+stream.on('error', error => console.log('Error', error.message));
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'styles');
+const projectFolder = path.join(__dirname, 'project-dist');
+const arr = [];
+
+fs.readdir(folder, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  fs.readdir(projectFolder, (err, projectFiles) => {
+    if (err) throw err;
+    projectFiles.forEach(projectFile => {
+      if (path.extname(projectFile) === '.css') {
+        fs.unlink(path.join(projectFolder, projectFile), err => {
+          if (err) throw err;
+        });
+      }
+    });
+    files.forEach(file => {
+      if (path.extname(file.name) === '.css') {
+        const name = file.name;
+        fs.readFile(`./05-merge-styles/styles/${name}`, 'utf-8', (err, dataFile) => {
+          if (err) throw err;
+          const data = dataFile.split('/n');
+          arr.push(data);
+          const data2 = arr.flat().join('');
+          fs.writeFile('./05-merge-styles/project-dist/bundle.css', data2, (err) => {
+            if (err) throw err;
+          });
+        });
+      }
+    });
+  });
+});
